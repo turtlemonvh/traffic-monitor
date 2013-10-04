b84148d545210b34612fe7ae068050e4ba648c33
Timothy Van Heest <timothy.vanheest@gtri.gatech.edu>
2013-10-04 09:13:56 -0400

    Added option for specifying directional routes

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ There should be a `routes.cfg` file in the directory where the utility is run.  
     [Routes]
     # List routes as urls
     # names used here will be included on lines of csv output file
-    home: https://maps.google.com/maps?saddr=Atlanta,+GA&daddr=Nashville,+TN&hl=en
+    route_a: https://maps.google.com/maps?saddr=Atlanta,+GA&daddr=Nashville,+TN&hl=en
 
     [Checktimes]
     # daysofweek = wd for week days, ad for all days
@@ -27,6 +27,14 @@ There should be a `routes.cfg` file in the directory where the utility is run.  
     eveningstart: 15
     eveningend: 20
 
+In the list of routes, adding `-morning` or `-evening` at the end of a route name will cause it to be calculated only for evening or morning periods.  This option should be used most of the time to specific the direction for coming and going.  An example "Routes" section for a commuter going between Atlanta and Nashville would look look like:
+
+    [Routes]
+    route_a-morning: https://maps.google.com/maps?saddr=Atlanta,+GA&daddr=Nashville,+TN&hl=en
+    route_a-evening: https://maps.google.com/maps?saddr=Nashville,+TN&daddr=Atlanta,+GA&hl=en
+
+If neither `-morning` nor `-evening` is included in a route name, the route is run for both times of day.  This may be useful for general analysis of traffic patterns.
+    
 ## Use
 
 Create the configuration file and set this up to run every few minutes with cron, [Windows Task Scheduler](http://support.microsoft.com/kb/308569), or a similar utility.  I suggest limiting yourself to less than every 10 minutes because the extra data isn't that useful and its more likely to get you noticed by Google.  We're screen scraping here, so lets be considerate.
@@ -34,9 +42,9 @@ For Windows Task Scheduler, make sure you run the command with a user account th
 
 An output file named `route_times.csv` will be placed in the directory where this is run.  It will look something like the following:
 
-    home,20131003,3,19:24,37
-    home,20131003,3,19:34,37
-    home,20131003,3,19:44,36
+    route_a,20131003,3,19:24,37
+    route_a,20131003,3,19:34,37
+    route_a,20131003,3,19:44,36
 
 The 1st column is the name of the route defined in your config file.  The 2nd is the date the route was calculated.  The 3rd is the day of the week where Monday is 0 and Sunday is 6.  The 4th is the time of day that the route was calculated, and the 5th and final column is the estimated time in minutes.
 
--- route_test.py
+++ route_test.py
@@ -8,49 +8,66 @@ from datetime import datetime
 import ConfigParser
 import string
 import time
+from collections import defaultdict
 
 """
 https://developers.google.com/maps/documentation/directions/#DirectionsRequests
-- Using the API, onlu biz customers can get route time in current traffic, so we need to scrape
+- Using the API, onlu biz customers can get route time in current traffic, 
+  so we need to scrape
 """
 
 def check_config():
   """ Read a configuration file to decide what to run"""
   config = ConfigParser.SafeConfigParser()
   config.read('routes.cfg')
-  routes = config.items('Routes')
+  # Get items as raw to avoid '%' interpolation
+  routes = config.items('Routes', True)
   
-  # Check day of week
+  # Reformat routes into a dictionary
+  formatted_routes = defaultdict(dict)
+  route_types = ['morning', 'evening']
+  for route in routes:
+    found = False
+    for route_type in route_types:
+      route_type_identifier = '-%s' %(route_type)
+      if route_type_identifier in route[0]:
+        route_name = route[0].split(route_type_identifier)[0]
+        formatted_routes[route_name][route_type] = route[1]
+        found = True
+        continue
+    if not found:
+      # This is not just a morning or evening route
+      # Assume its for both coming and going
+      route_name = route[0]
+      for route_type in route_types:
+        # Add explicit entries for morning and evening routes
+        formatted_routes[route_name][route_type] = route[1]
+      
+  # Check day of week to see if calculation is needed
   daysofweek = config.get('Checktimes', 'daysofweek')
   if daysofweek == 'ad':
     pass
   elif daysofweek == 'wd':
     if not(datetime.today().weekday() < 5):
-      return []
-  else:
-    raise Error("Invalid configuation: 'daysofweek' must be 'ad' or 'wd'")
-
-  # Check day of week
-  daysofweek = config.get('Checktimes', 'daysofweek')
-  if daysofweek == 'ad':
-    pass
-  elif daysofweek == 'wd':
-    if not(datetime.today().weekday() < 5):
-      print "Not running because %s is not a valid day of week" %(datetime.today().weekday())
-      return []
+      return ({}, None)
   else:
     raise Error("Invalid configuation: 'daysofweek' must be 'ad' or 'wd'")
   
-  # Check time of day
+  # Check time of day to find which route to run, if any
+  route_type = None
   hour = int(datetime.now().strftime('%H'))
-  if (hour < config.getint('Checktimes', 'morningstart') or
-     hour >= config.getint('Checktimes', 'eveningend') or
-     (hour >= config.getint('Checktimes', 'morningend') and
-      hour < config.getint('Checktimes', 'eveningstart'))):
-      print "Not running because %d is not a valid time of day" %(hour)
-      return []
+  if (hour > config.getint('Checktimes', 'morningstart') and
+      hour <= config.getint('Checktimes', 'morningend')):
+    route_type = 'morning'
+  elif (hour > config.getint('Checktimes', 'eveningstart') and
+      hour <= config.getint('Checktimes', 'eveningend')):
+    route_type = 'evening'
+  else:
+    print ("Not running because %d is not within specified time ",
+           "ranges for morning and evening commutes") %(hour)
+    return ({}, None)
 
-  return routes
+  return (formatted_routes, route_type)
 
 def calculate_route_time(route):
   """Take a url representing a route.  Return time information. """
@@ -58,10 +75,15 @@ def calculate_route_time(route):
   url = route[1]
   q = urllib2.urlopen(url)
   response = q.read()
-  f1 = string.find(response, 'id="panel_dir"') # Find side panel
-  f2 = string.find(response[f1:-1], '<div class="altroute-rcol altroute-aux">') # Find first route alternative within side panel
-  result = response[f1+f2:f1+f2+200] # get section of html around this div
-  time_data = re.search('<span>(.*)</span>', result).group(1).split('traffic: ')[1] # scrape out time component from this data
+  f1 = string.find(response, 'id="panel_dir"')  # Find side panel
+  # Find first route alternative within side panel
+  f2 = string.find(response[f1:-1],
+    '<div class="altroute-rcol altroute-aux">')
+  # get section of html around this div
+  result = response[f1+f2:f1+f2+200]
+  # scrape out time component from this data
+  time_data = (re.search('<span>(.*)</span>', result)
+    .group(1).split('traffic: ')[1])
   
   # Calculate time in minutes
   mins = -1
@@ -72,17 +94,22 @@ def calculate_route_time(route):
     mins = int(re.search('(\d+) min', time_data).group(1))
 
   # Get together object for output
-  day_of_week = datetime.today().weekday() # 0 for Monday, 6 for Sunday
-  date = datetime.today().strftime('%Y%m%d') # E.g. 20131003
-  time_of_day = datetime.now().strftime("%H:%M") # E.g. 15:30
+  day_of_week = datetime.today().weekday()  # 0 for Monday, 6 for Sunday
+  date = datetime.today().strftime('%Y%m%d')  # E.g. 20131003
+  time_of_day = datetime.now().strftime("%H:%M")  # E.g. 15:30
   return (route_name, date, day_of_week, time_of_day, mins)
 
 if __name__ == "__main__":
   """Main script """
-  routes = check_config()
+  (routes, route_type) = check_config()
   with open('route_times.csv', 'ab+') as csvfile:
     writer = csv.writer(csvfile)
-    for route in routes:
-      writer.writerow(calculate_route_time(route))
-      time.sleep(2) # wait a couple seconds so we don't freak Google out
+    for route_name, route_urls in routes.items():
+      if route_type in route_urls:
+        # skip this if not specified
+        # allows user to do just morning or evening routes
+        writer.writerow(calculate_route_time(
+          (route_name, route_urls[route_type])))
+        # wait a couple seconds so we don't freak Google out
+        time.sleep(2)
   
